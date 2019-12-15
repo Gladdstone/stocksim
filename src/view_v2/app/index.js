2dd8395ae332db547e7c9266065c4db72bc84ed6
Joe Farrell <disastrousgamemaster@gmail.com>
2019-12-15 02:30:44 -0500

    Switch based routing

--- src/view_v2/app/index.js
+++ src/view_v2/app/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import App from './App';
 import Login from './login/login.jsx';
 import Registration from './registration/registration.jsx';
+import Error404 from './Error404/Error404.jsx';
 
 import './index.less';
 
@@ -22,9 +23,12 @@ const routing = (
                     <Link to="/register">Register</Link>
                 </li>
             </ul>
-            <Route path="/" component={App} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Registration} />
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Registration} />
+                <Route component={Error404} />
+            </Switch>
         </div>
     </Router>
 );
